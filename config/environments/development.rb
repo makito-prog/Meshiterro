fcf8519a91beb782d98b6313be64ab7d054a56eb
makito-prog <147535008+makito-prog@users.noreply.github.com>
2024-09-23 07:39:34 +0000

    [Finish] 4-1

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -74,4 +74,6 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
   config.hosts << "c285e062891c4ae3baf2a9dcbfdb1b3a.vfs.cloud9.ap-northeast-1.amazonaws.com"
+  config.active_job.queue_adapter = :inline
+
 end
